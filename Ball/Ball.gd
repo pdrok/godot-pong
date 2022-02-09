39a9fd5290451f04648625c15495b497fdcfc83d
Pedro Lopez <plopez@poweranalytics.com>
2022-02-09 07:37:33 -0300

    making the ball bounce

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -9,4 +9,6 @@ func _ready():
 	velocity.y = [-0.8,0.8][randi() % 2]
 	
 func _physics_process(delta):
-	move_and_slide(velocity * speed)
+	var collision_object = move_and_collide(velocity * speed * delta)
+	if collision_object:
+		velocity = velocity.bounce(collision_object.normal)
